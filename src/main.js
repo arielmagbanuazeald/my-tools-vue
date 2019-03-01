25c6095aee39d6bb60d6334f672dd9b1abb7ef39
Dash10-CPU009 <ariel@zeald>
2019-03-01 13:23:46 +0800

    Applied Material drawer.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
-Vue.config.productionTip = false
+import {MdDrawer, MdToolbar, MdButton, MdList, MdIcon, MdContent} from 'vue-material/dist/components/'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdDrawer);
+Vue.use(MdToolbar);
+Vue.use(MdButton);
+Vue.use(MdList);
+Vue.use(MdIcon);
+Vue.use(MdContent);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
